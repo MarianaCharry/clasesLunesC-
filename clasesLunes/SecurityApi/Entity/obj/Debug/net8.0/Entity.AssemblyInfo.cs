797b86e4ea91132282cf389466da7c0f9c439413
mariana <marianamayaya@gmail.com>
2024-07-04 07:26:12 -0500

    Cambios

--- clasesLunes/SecurityApi/Entity/obj/Debug/net8.0/Entity.AssemblyInfo.cs
+++ clasesLunes/SecurityApi/Entity/obj/Debug/net8.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccd1caaf6a9855d71c20fc05a86d8ac94f48237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef4271806667f8b691d637e8fe5679552e58b25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
